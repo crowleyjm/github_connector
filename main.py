4c111c929441955f9dca7fcbac17285a6a5a84af
RichJ1129 <richjoseph1129@gmail.com>
2020-07-09 18:40:04 -0400

    Name changed

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 
 
-@app.route('')
+@app.route('/')
 def home():
     return render_template('welcome/welcome.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(threaded=True, port=5000)
