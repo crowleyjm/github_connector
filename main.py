0ee2cdadbd48c8117067d5487b01dbd98ffc72fe
RichJ1129 <richjoseph1129@gmail.com>
2020-07-09 17:56:27 -0400

    Tenplate of welcome page created. Alongside base.html with header.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, Blueprint, render_template
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('welcome/welcome.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
