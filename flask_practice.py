eb1558a8020ea9f2b849470af1f930fb216f0a7e
Adam Conrad <conraada@oregonstate.edu>
2020-07-10 13:32:12 -0700

    Basic registration dialog

---
+++ flask_practice.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     text = request.form['uname']
+#     processed_text = text.upper()
+#     return processed_text
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
