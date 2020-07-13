e48c0bbf966749e715d761f2abd8a002ad3af0f9
RichJ1129 <richjoseph1129@gmail.com>
2020-07-12 23:56:22 -0400

    profile_build.py added. Gets all the repositories in users GitHub and looks through them for languages. Stores language usage in a dictionary

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def home():
     return render_template('welcome/welcome.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- profile_build.py
+++ profile_build.py
@@ -4,20 +4,36 @@ import json
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
-github_url = 'https://api.github.com'
-
-
 @bp.route('/<git_name>')
 def user_get_lang(git_name):
     github_url = 'https://api.github.com/users/' + git_name + '/repos'
-    print(github_url)
     payload = {}
-    headers = {}
+    headers = {
+        # 'Authorization': 'Bearer Enter Token Here'
+    }
 
     response = requests.request("GET", github_url, headers=headers, data=payload)
+    language_dict = {}
 
+    new_bytes = response.content
+    new_json = json.loads(new_bytes)
 
-    return response.content
+    for i in range(len(new_json)):
+        new_url = new_json[i]["languages_url"]
+        new_payload = {}
+        new_url_response = requests.request("GET", new_url, headers=headers, data=new_payload)
+
+        lang_cont = new_url_response.content
+        lang_json = json.loads(lang_cont)
+
+        for lang_name in lang_json:
+            new_val = lang_json[lang_name]
+            if lang_name not in language_dict:
+                language_dict[lang_name] = new_val
+            else:
+                language_dict[lang_name] += new_val
+
+    return language_dict
 
 
 
