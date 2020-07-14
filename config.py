315bbc61079492d2afdb0b22abe7de2a92f26b2f
Jillian Crowley <crowleji@oregonstate.edu>
2020-07-14 19:46:15 -0400

    add github authentication functionality, html edits

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
-    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/users"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:se2020@localhost:5432/github_connector"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/users"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
