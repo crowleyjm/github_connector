1379c91a05101bbc69d02c8f59be96ce9b5ebdb5
Jillian Crowley <crowleji@oregonstate.edu>
2020-07-14 12:19:49 -0400

    first commit

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/users"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/users"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:se2020@localhost:5432/github_connector"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
