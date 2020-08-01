04538508fd89d4f51320eab1b5db29141439f4af
RichJ1129 <richjoseph1129@gmail.com>
2020-07-31 23:59:21 -0400

    Posts in profile page

--- app/models.py
+++ app/models.py
@@ -36,7 +36,7 @@ class Comment(db.Model):
             return ''
 
     def __repr__(self):
-        return self.message
+        return '<Post {}>'.format(self.message)
 
     def __init__(self, message, user_id):
         self.message = message
