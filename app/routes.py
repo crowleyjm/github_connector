f7ed9d5fdc0010e2455113ee76421f0d0d7cfd70
RichJ1129 <richjoseph1129@gmail.com>
2020-07-31 23:10:36 -0400

    Avatar implementation

--- app/routes.py
+++ app/routes.py
@@ -200,6 +200,6 @@ def about():
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', user=current_user)
 
 
