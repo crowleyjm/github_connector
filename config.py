316e6365aaaff9bcf198cc04ab495b5f3e6ec543
RichJ1129 <richjoseph1129@gmail.com>
2020-08-13 19:26:51 -0400

    Like Unlike

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class Config(object):
     CONNECTIONS_PER_PAGE = 5
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
 
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = "postgres://ovsoaajreppuyb:84014559fb7014e2af3c263d4e30f1afb37196a7d511cd882df4a8302ba1891d@ec2-34-224-229-81.compute-1.amazonaws.com:5432/dfq49p3g24hncm"
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
