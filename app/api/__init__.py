4e4dc840b7883f126ac78fac6d6c34ec5136e3c3
RichJ1129 <richjoseph1129@gmail.com>
2020-08-13 18:53:41 -0400

    Environment Variables

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,12 +4,7 @@ import os
 bp = Blueprint('api', __name__)
 
 
-github_blueprint = make_github_blueprint(client_id=os.environ.get('CLIENT_ID', None), client_secret=os.environ.get('CLIENT_SECRET', None))
-
-# def create_app(config_class=Config):
-#     app = Flask(__name__)
-#
-#     from app.api import bp as api_bp
-#     app.register_blueprint(api_bp, url_prefix='/api')
+github_blueprint = make_github_blueprint(client_id=os.environ.get('CLIENT_ID', None),
+                                         client_secret=os.environ.get('CLIENT_SECRET', None))
 
 from app.api import users
